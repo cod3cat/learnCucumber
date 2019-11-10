fc70479e7e0be6fde594ac3f39600cf817976a28
Praveen G <25690316+cod3cat@users.noreply.github.com>
2019-11-10 16:48:13 +0530

    Added usage of hooks and using it to print scenario name and status

--- src/test/java/LearnCucumber/runner/SingleParameterRunner.java
+++ src/test/java/LearnCucumber/runner/SingleParameterRunner.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = {"src/test/java/LearnCucumber/featureFiles/SingleParameter.feature"},
-		glue = {"LearnCucumber/steps"},
+		glue = {"LearnCucumber/steps", "LearnCucumber/hooks"},
 		monochrome = true,
 		dryRun = false
 		)
